51e9fb2413b90f2c98d95c279385c20b1699831d
ManideepUnplat <manideep.gangishetty@unplatforms.com>
2023-02-13 15:18:45 +0530

    'commit'

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -13,7 +13,7 @@ function App() {
       <div className='p-3'>
         <AddToTodo/>
         <CurrentEmail/>
-        <EmailAnalytics/>
+        {/* <EmailAnalytics/> */}
         <PerformanceTools/>
       </div>
     </div>
